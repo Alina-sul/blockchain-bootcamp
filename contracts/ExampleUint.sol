7a3d9290b3426b34bc81c5289848f01fa692528a
Alina-sul <alina.sulkovsky@gmail.com>
2023-03-03 17:59:49 +0200

    uint

--- contracts/ExampleUint.sol
+++ contracts/ExampleUint.sol
@@ -7,6 +7,8 @@ contract ExampleUint {
     uint public myUint; // default value is 0
     // uint === uint256
 
+    int public myInt = -10; //-2^128 to +2^128-1
+
     function setMyUint(uint _myUint) public {
         myUint = _myUint; // range 0 - (2^256)-1 
     }
